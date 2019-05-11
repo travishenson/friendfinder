663dff16f21cb7007e883b7c3da96f607d61f59f
Travis Henson <travisghenson@gmail.com>
2019-05-10 22:43:45 -0400

    Compatibility logic implemented. Res sent to front end properly.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,112 +3,112 @@ module.exports = [
     name: 'Steve Rogers',
     photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/d/d7/CapAmerica-EndgameProfile.jpg/',
     scores: [
-      "3",
-      "3",
-      "3",
-      "2",
-      "2",
-      "1",
-      "4",
-      "3",
-      "2",
-      "3"
+      3,
+      3,
+      3,
+      2,
+      2,
+      1,
+      4,
+      3,
+      2,
+      3
     ]
   },
   {
     name: 'Tony Stark',
     photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/3/35/IronMan-EndgameProfile.jpg',
     scores: [
-      "4",
-      "4",
-      "2",
-      "3",
-      "2",
-      "2",
-      "3",
-      "2",
-      "4",
-      "5"
+      4,
+      4,
+      2,
+      3,
+      2,
+      2,
+      3,
+      2,
+      4,
+      5
     ]
   },
   {
     name: 'Thor',
     photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/1/13/Thor-EndgameProfile.jpg/',
     scores: [
-      "4",
-      "3",
-      "4",
-      "1",
-      "5",
-      "2",
-      "5",
-      "3",
-      "1",
-      "4"
+      4,
+      3,
+      4,
+      1,
+      5,
+      2,
+      5,
+      3,
+      1,
+      4
     ]
   },
   {
     name: 'Natasha Romanoff',
     photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/9a/BlackWidow-EndgameProfile.jpg/',
     scores: [
-      "3",
-      "3",
-      "4",
-      "2",
-      "2",
-      "1",
-      "3",
-      "2",
-      "2",
-      "3"
+      3,
+      3,
+      4,
+      2,
+      2,
+      1,
+      3,
+      2,
+      2,
+      3
     ]
   },
   {
     name: 'Clint Barton',
     photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/08/HawkeyeRonin-EndgameProfile.jpg/',
     scores: [
-      "5",
-      "2",
-      "2",
-      "2",
-      "4",
-      "1",
-      "3",
-      "2",
-      "5",
-      "5"
+      5,
+      2,
+      2,
+      2,
+      4,
+      1,
+      3,
+      2,
+      5,
+      5
     ]
   },
   {
-    name: 'T\'Challa',
+    name: "T'Challa",
     photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/99/Black_Panther_AIW_Profile.jpg/',
     scores: [
-      "4",
-      "2",
-      "5",
-      "1",
-      "3",
-      "2",
-      "2",
-      "1",
-      "3",
-      "2"
+      4,
+      2,
+      5,
+      1,
+      3,
+      2,
+      2,
+      1,
+      3,
+      2
     ]
   },
   {
     name: 'Stephen Strange',
     photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/d/d9/Doctor_Strange_AIW_Profile.jpg/',
     scores: [
-      "5",
-      "1",
-      "4",
-      "4",
-      "5",
-      "1",
-      "2",
-      "5",
-      "4",
-      "1"
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
     ]
   }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,38 @@ module.exports = function(app) {
     res.json(friends);
   });
 
-  app.post('/api/friends', function(req, res) {
+  app.post('/api/friends', function (req, res) {
+    
+    var newFriend = req.body;
+    var newFriendScores = newFriend.scores.map(Number);
+
+    var differenceArr = [];
+    
+    friends.forEach(function (currentFriend) {
+      var currentFriendScores = currentFriend.scores;
+      var currentFriendName = currentFriend.name
+      var totalDifference = 0;
+      currentFriendScores.forEach(function (currentScore, index) {
+        totalDifference += Math.abs(currentScore - newFriendScores[index]);
+      })
+      var differenceData = { 
+        name: currentFriendName,
+        difference: totalDifference
+      };
+      differenceArr.push(differenceData);
+    });
+
+    differenceArr.sort(function (a, b) {
+      return a.difference - b.difference;
+    });
+
+    var userMatch = differenceArr[0].name;
+    console.log(userMatch);
+
+    var userMatchData = friends.find(object => object.name === userMatch);
+    console.log(userMatchData);
+
     friends.push(req.body);
-    res.json(req.body);
+    res.json(userMatchData);
   });
 };
